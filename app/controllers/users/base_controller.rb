9b61181afdf1ff286ca4431aa684948cb82cb0c2
Nicolae Rotaru <nicolaer@saltedge.com>
2019-01-11 15:12:14 +0200

    UI

--- app/controllers/users/base_controller.rb
+++ app/controllers/users/base_controller.rb
@@ -1,4 +1,13 @@
 class Users::BaseController < ApplicationController
   before_action :authenticate_user!
   layout "application"
+  helper_method :user_shard
+
+  private
+
+  def user_shard
+    Switch.with_master(Switch.master_shard) do
+      LookupUser.find_by(id: current_user.id).shard
+    end
+  end
 end
