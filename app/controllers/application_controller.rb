f722d1a6bf51f18570fb21e0d106bbf15aae6b04
Nicolae Rotaru <nicolaer@saltedge.com>
2019-01-16 14:49:51 +0200

    session[:user_id] set after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  def after_sign_in_path
-    session[:user_id] = current_user.id
+  def after_sign_in_path_for(resource)
+    session[:user_id] = resource.id
     users_connections_path
   end
 end
